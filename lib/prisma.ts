92b2380755b6088dd8b7d8bfbd52043ba089f698
Hekman <yayakinetic@gmail.com>
2024-08-15 23:43:16 +0800

    environment set up.

---
+++ lib/prisma.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// import { PrismaClient } from '@prisma/client'
+// const prisma = new PrismaClient()
+
+// async function main() {
+//   const users = await prisma.user.findMany()
+//   console.log(users)
+// }
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
